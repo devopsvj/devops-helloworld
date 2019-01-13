6f8195176c2782b60901ae6b3696f36ca2832cf1
devopsvj <devopsvj@gmail.com>
2019-01-13 20:58:06 +0000

    Added test from linux Server

--- sample_code.sh
+++ sample_code.sh
@@ -2,6 +2,7 @@
 #
 
 echo "This is an Samples Script from GitHub"
+echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
